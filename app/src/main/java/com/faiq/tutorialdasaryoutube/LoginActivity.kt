c3e957afedb01df30fe1d52d5a0da84df9a892b8
faiq526 <callbackfaiq1@gmail.com>
2023-11-01 15:34:39 +0700

    Faiq Haidar Dzaky - Interaksi User

--- app/src/main/java/com/faiq/tutorialdasaryoutube/LoginActivity.kt
+++ app/src/main/java/com/faiq/tutorialdasaryoutube/LoginActivity.kt
@@ -1,11 +1,35 @@
 package com.faiq.tutorialdasaryoutube
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginActivity: AppCompatActivity {
+
+class LoginActivity: AppCompatActivity() {
+    lateinit var btnlogin: Button
+    lateinit var etEmail: EditText
+    lateinit var etPassword: EditText
+    lateinit var email: String
+    lateinit var password : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_linear)
+
+        btnlogin = findViewById(R.id.btn_login)
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+
+        btnlogin.setOnClickListener {
+            email = etEmail.text.toString()
+            password = etPassword.text.toString()
+            if (email.isNullOrEmpty() && password.isNullOrEmpty()) {
+                Toast.makeText(applicationContext, "Email atau password masih kosong", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Sukses! Anda sudah memasukkan Email dan Password", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
